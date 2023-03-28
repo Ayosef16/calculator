2f04c509e0c9d2cbc793fcfbf29c51e5ef6cdc38
Ayosef <syrivera@uc.cl>
2023-03-27 22:02:18 -0300

    fix some bugs in the functions

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const currentDisplay = document.querySelector('.current-display');
 const previousDisplay = document.querySelector('.previous-display');
-const number = document.querySelectorAll('.number');
-const operator = document.querySelectorAll('.operator');
-const equal = document.querySelector('#equal');
-const clear = document.querySelector('#clear');
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalButton = document.querySelector('#equal');
+const clearButton = document.querySelector('#clear');
+const deleteButton = document.querySelector('#delete');
 
 
 
@@ -25,7 +26,7 @@ function divide(a,b) {
 
 let firstNumber = '';
 let secondNumber = '';
-let firstOperator = '';
+let operator = '';
 
 
 function operate(operator,a,b) {
@@ -42,30 +43,73 @@ function operate(operator,a,b) {
         case '÷':
             return divide(a,b);
             break;
+        default:
+            return
     }
 };
 
-function changeDisplay(e) {
-    firstNumber += e.target.textContent;
-    currentDisplay.textContent = firstNumber;
+function appendNumber(number) {
+    if(number === '.' && firstNumber.includes('.')) return
+    firstNumber += number;
 };
 
-function getOperator(e) {
-    firstOperator = e.target.textContent;
-    previousDisplay.textContent = `${firstNumber} ${firstOperator}`;
-    currentDisplay.textContent = 0;
+function deleteNumber() {
+    firstNumber = firstNumber.toString().slice(0,-1);
+}
+
+function updateDisplay() {
+    currentDisplay.textContent = firstNumber;
+    if( operator != null) {
+        previousDisplay.textContent = `${secondNumber} ${operator}`
+    }
+};
+
+function chooseOperator(sign) {
+    if(firstNumber === '') return
+    if(secondNumber !== '') {
+        getResult();
+    }
+    operator = sign;
+    secondNumber = firstNumber;
+    firstNumber = '';
 };
 
 function getResult() {
-    let answer = operate(first,second,sign);
-    display.textContent = answer;
+    currentDisplay.textContent = operate(operator,parseFloat(secondNumber),parseFloat(firstNumber));
+    firstNumber = currentDisplay.textContent;
+    operator = undefined;
+    secondNumber = '';
 };
 
 function clearResult() {
-    window.location.reload();
+    currentDisplay.textContent = '';
+    previousDisplay.textContent = '';
+    firstNumber = '';
+    secondNumber = '';
+    operator = undefined;
 };
 
-number.forEach(number => number.addEventListener('click',changeDisplay));
-operator.forEach(operator => operator.addEventListener('click', getOperator));
-equal.addEventListener('click', getResult);
-clear.addEventListener('click',clearResult);
+numberButtons.forEach(button => button.addEventListener('click', () => {
+    appendNumber(button.textContent);
+    updateDisplay();
+}));
+
+operatorButtons.forEach(button => button.addEventListener('click', () => {
+    chooseOperator(button.textContent);
+    updateDisplay();
+}));
+
+equalButton.addEventListener('click', () => {
+    getResult();
+});
+
+clearButton.addEventListener('click', () => {
+    clearResult();
+    updateDisplay();
+});
+
+deleteButton.addEventListener('click', () => {
+    deleteNumber();
+    updateDisplay();
+});
+
