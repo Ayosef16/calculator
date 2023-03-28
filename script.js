23f06003fedb5ec9f243a442f31f65eda67e4896
Ayosef <syrivera@uc.cl>
2023-03-28 09:33:15 -0300

    Clean some of the js code

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function divide(a,b) {
 
 let firstNumber = '';
 let secondNumber = '';
-let operator = '';
+let operator = undefined;
 
 
 function operate(operator,a,b) {
@@ -48,38 +48,52 @@ function operate(operator,a,b) {
     }
 };
 
+//Create a function that can append the numbers
+
 function appendNumber(number) {
-    if(number === '.' && firstNumber.includes('.')) return
-    firstNumber += number;
+    if(number.textContent === '.' && firstNumber.toString().includes('.')) return
+    firstNumber += number.textContent;
 };
 
-function deleteNumber() {
-    firstNumber = firstNumber.toString().slice(0,-1);
-}
+//Make a update display function, that changes what is showing in the screen
 
 function updateDisplay() {
     currentDisplay.textContent = firstNumber;
-    if( operator != null) {
-        previousDisplay.textContent = `${secondNumber} ${operator}`
+    if (secondNumber !== '') {
+        previousDisplay.textContent = `${secondNumber} ${operator}`;
     }
 };
 
-function chooseOperator(sign) {
-    if(firstNumber === '') return
-    if(secondNumber !== '') {
+//Make a function that when u choose an operator, it moves the first input and the operator to a secondary display
+
+function chooseOperator(op) {
+    if (firstNumber === '') return
+    if (secondNumber !== '') {
         getResult();
     }
-    operator = sign;
+    operator = op.textContent;
     secondNumber = firstNumber;
     firstNumber = '';
 };
 
+//Make a function to show the result on the current display
+
 function getResult() {
-    currentDisplay.textContent = operate(operator,parseFloat(secondNumber),parseFloat(firstNumber));
-    firstNumber = currentDisplay.textContent;
-    operator = undefined;
-    secondNumber = '';
+    if(operator === '÷' && parseFloat(firstNumber) === 0) {
+        alert("can't divide by zero!")
+        clearResult();
+        return
+    }
+    if(firstNumber !== '' && secondNumber !== '' && operator !== undefined) {
+        let firstScreen = parseFloat(firstNumber);
+        let secondScreen = parseFloat(secondNumber);
+        firstNumber = +operate(operator,secondScreen,firstScreen).toFixed(3);
+        secondNumber = '';
+        operator = undefined;
+    }
 };
+
+//Make a function to clear the calculator
 
 function clearResult() {
     currentDisplay.textContent = '';
@@ -89,18 +103,27 @@ function clearResult() {
     operator = undefined;
 };
 
+//Make a function to delete the last number
+
+function deleteNumber() {
+    firstNumber = firstNumber.toString().slice(0,-1);
+};
+
+//Make the event listener events
+
 numberButtons.forEach(button => button.addEventListener('click', () => {
-    appendNumber(button.textContent);
+    appendNumber(button);
     updateDisplay();
 }));
 
 operatorButtons.forEach(button => button.addEventListener('click', () => {
-    chooseOperator(button.textContent);
+    chooseOperator(button);
     updateDisplay();
 }));
 
 equalButton.addEventListener('click', () => {
     getResult();
+    updateDisplay();
 });
 
 clearButton.addEventListener('click', () => {
